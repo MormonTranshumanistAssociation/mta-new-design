d89520ed36d41cbec31e5fb57d2b944c5ce696ea
Carl Youngblood <carl@blockscale.net>
2022-02-06 18:22:16 -0700

    Modernize javascript

--- js/parallax.js
+++ js/parallax.js
@@ -1,195 +1,114 @@
-/**
- * Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// console.log("load parallax");
-
 function initializeParallax(clip) {
-  // console.log("initializeParallax");
-  var parallax = clip.querySelectorAll("*[parallax]");
-  var parallaxDetails = [];
-  var sticky = false;
+  const parallax = clip.querySelectorAll('*[parallax]');
+  const parallaxDetails = [];
+  let sticky = false;
 
-  // Edge requires a transform on the document body and a fixed position element
-  // in order for it to properly render the parallax effect as you scroll.
-  // See https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/5084491/
-  if (getComputedStyle(document.body).transform == "none")
-    document.body.style.transform = "translateZ(0)";
-  var fixedPos = document.createElement("div");
-  fixedPos.style.position = "fixed";
-  fixedPos.style.top = "0";
-  fixedPos.style.width = "1px";
-  fixedPos.style.height = "1px";
+  if (getComputedStyle(document.body).transform === 'none') {
+    document.body.style.transform = 'translateZ(0)';
+  }
+  const fixedPos = document.createElement('div');
+  fixedPos.style.position = 'fixed';
+  fixedPos.style.top = '0';
+  fixedPos.style.width = '1px';
+  fixedPos.style.height = '1px';
   fixedPos.style.zIndex = 1;
   document.body.insertBefore(fixedPos, document.body.firstChild);
 
-  for (var i = 0; i < parallax.length; i++) {
-    var elem = parallax[i];
-    var container = elem.parentNode;
-    if (getComputedStyle(container).overflow != "visible") {
-      console.error(
-        "Need non-scrollable container to apply perspective for",
-        elem
-      );
-      continue;
+  parallax.forEach(elem => {
+    const container = elem.parentNode;
+    const containerParent = container.parentNode;
+    if (getComputedStyle(container).overflow !== 'visible') {
+      console.error('Need non-scrollable container to apply perspective for', elem);
+      return;
     }
-    if (clip && container.parentNode != clip) {
-      console.warn(
-        "Currently we only track a single overflow clip, but elements from multiple clips found.",
-        elem
-      );
+    if (clip && container.parentNode !== clip) {
+      console.warn('Currently we only track a single overflow clip, but elements from multiple clips found.', elem);
     }
-    var clip = container.parentNode;
-    if (getComputedStyle(clip).overflow == "visible") {
-      console.error(
-        "Parent of sticky container should be scrollable element",
-        elem
-      );
+    if (getComputedStyle(containerParent).overflow === 'visible') {
+      console.error('Parent of sticky container should be scrollable element', containerParent);
     }
     // TODO(flackr): optimize to not redo this for the same clip/container.
-    var perspectiveElement;
-    if (sticky || getComputedStyle(clip).webkitOverflowScrolling) {
+    let perspectiveElement;
+    if (sticky || getComputedStyle(containerParent).webkitOverflowScrolling) {
       sticky = true;
       perspectiveElement = container;
     } else {
-      perspectiveElement = clip;
-      container.style.transformStyle = "preserve-3d";
+      perspectiveElement = containerParent;
+      container.style.transformStyle = 'preserve-3d';
     }
-    perspectiveElement.style.perspectiveOrigin = "bottom right";
-    perspectiveElement.style.perspective = "1px";
-    if (sticky) elem.style.position = "-webkit-sticky";
-    if (sticky) elem.style.top = "0";
-    elem.style.transformOrigin = "bottom right";
+    perspectiveElement.style.perspectiveOrigin = 'bottom right';
+    perspectiveElement.style.perspective = '1px';
+    if (sticky) {
+      elem.style.position = '-webkit-sticky';
+      elem.style.top = '0';
+    }
+    elem.style.transformOrigin = 'bottom right';
 
     // Find the previous and next elements to parallax between.
-    var previousCover = parallax[i].previousElementSibling;
-    while (previousCover && previousCover.hasAttribute("parallax"))
+    let previousCover = elem.previousElementSibling;
+    while (previousCover && previousCover.hasAttribute('parallax')) {
       previousCover = previousCover.previousElementSibling;
-    var nextCover = parallax[i].nextElementSibling;
-    while (nextCover && !nextCover.hasAttribute("parallax-cover"))
+    }
+    let nextCover = elem.nextElementSibling;
+    while (nextCover && !nextCover.hasAttribute('parallax-cover')) {
       nextCover = nextCover.nextElementSibling;
-
-    parallaxDetails.push({
-      node: parallax[i],
-      top: parallax[i].offsetTop,
-      sticky: !!sticky,
-      nextCover: nextCover,
-      previousCover: previousCover,
-    });
-  }
+    }
+    parallaxDetails.push({'node': elem,
+                          'top': elem.offsetTop,
+                          'sticky': !!sticky,
+                          'nextCover': nextCover,
+                          'previousCover': previousCover});
+  });
 
   // Add a scroll listener to hide perspective elements when they should no
   // longer be visible.
-  clip.addEventListener("scroll", function () {
-    for (var i = 0; i < parallaxDetails.length; i++) {
-      var container = parallaxDetails[i].node.parentNode;
-      var previousCover = parallaxDetails[i].previousCover;
-      var nextCover = parallaxDetails[i].nextCover;
-      var parallaxStart = previousCover
-        ? previousCover.offsetTop + previousCover.offsetHeight
-        : 0;
-      var parallaxEnd = nextCover
-        ? nextCover.offsetTop
-        : container.offsetHeight;
-      var threshold = 200;
-      var visible =
-        parallaxStart - threshold - clip.clientHeight < clip.scrollTop &&
-        parallaxEnd + threshold > clip.scrollTop;
-      // FIXME: Repainting the images while scrolling can cause jank.
-      // For now, keep them all.
-      // var display = visible ? 'block' : 'none'
-      var display = "block";
-      if (parallaxDetails[i].node.style.display != display)
-        parallaxDetails[i].node.style.display = display;
-    }
+  clip.addEventListener('scroll', function() {
+    parallaxDetails.forEach(elem => {
+      const container = elem.node.parentNode;
+      const previousCover = elem.previousCover;
+      const nextCover = elem.nextCover;
+      const parallaxStart = previousCover ? (previousCover.offsetTop + previousCover.offsetHeight) : 0;
+      const parallaxEnd = nextCover ? nextCover.offsetTop : container.offsetHeight;
+      const threshold = 200;
+      const visible = parallaxStart - threshold - clip.clientHeight < clip.scrollTop && parallaxEnd + threshold > clip.scrollTop;
+      elem.node.style.display = visible ? 'block' : 'none';
+    });
   });
-  window.addEventListener("resize", onResize.bind(null, parallaxDetails));
+  window.addEventListener('resize', onResize.bind(null, parallaxDetails));
   onResize(parallaxDetails);
-  for (var i = 0; i < parallax.length; i++) {
-    parallax[i].parentNode.insertBefore(
-      parallax[i],
-      parallax[i].parentNode.firstChild
-    );
-  }
+  parallax.forEach(elem => {
+    elem.parentNode.insertBefore(elem, elem.parentNode.firstChild);
+  });
 }
 
 function onResize(details) {
-  // console.log({ details }, "onResize");
-
-  for (var i = 0; i < details.length; i++) {
-    var container = details[i].node.parentNode;
-
-    var clip = container.parentNode;
-    var previousCover = details[i].previousCover;
-    var nextCover = details[i].nextCover;
-    var rate = details[i].node.getAttribute("parallax");
-
-    var parallaxStart = previousCover
-      ? previousCover.offsetTop + previousCover.offsetHeight
-      : 0;
-    var scrollbarWidth = details[i].sticky
-      ? 0
-      : clip.offsetWidth - clip.clientWidth;
-    var parallaxElem = details[i].sticky ? container : clip;
-    var height = details[i].node.offsetHeight;
-    var depth = 0;
+  details.forEach(detail => {
+    const container = detail.node.parentNode;
+    const clip = container.parentNode;
+    const previousCover = detail.previousCover;
+    const nextCover = detail.nextCover;
+    const rate = detail.node.getAttribute('parallax');
+    const parallaxStart = previousCover ? (previousCover.offsetTop + previousCover.offsetHeight) : 0;
+    const scrollbarWidth = details.sticky ? 0 : clip.offsetWidth - clip.clientWidth;
+    const parallaxElem = detail.sticky ? container : clip;
+    const height = detail.node.offsetHeight;
+    let depth = 0;
     if (rate) {
-      depth = 1 - 1 / rate;
+      depth = 1 - (1 / rate);
     } else {
-      var parallaxEnd = nextCover
-        ? nextCover.offsetTop
-        : container.offsetHeight;
-      depth =
-        (height - parallaxEnd + parallaxStart) / (height - clip.clientHeight);
+      const parallaxEnd = nextCover ? nextCover.offsetTop : container.offsetHeight;
+      depth = (height - parallaxEnd + parallaxStart) / (height - clip.clientHeight);
     }
-    if (details[i].sticky) depth = 1.0 / depth;
-
-    var scale = 1.0 / (1.0 - depth);
-
+    if (detail.sticky) {
+      depth = 1.0 / depth;
+    }
+    const scale = 1.0 / (1.0 - depth);
     // The scrollbar is included in the 'bottom right' perspective origin.
-    var dx = scrollbarWidth * (scale - 1);
+    const dx = scrollbarWidth * (scale - 1);
     // Offset for the position within the container.
-    var dy = details[i].sticky
-      ? -(clip.scrollHeight - parallaxStart - height) * (1 - scale)
-      : (parallaxStart - depth * (height - clip.clientHeight)) * scale;
-
-    var css =
-      "scale(" +
-      (1 - depth) +
-      ") translate3d(" +
-      dx +
-      "px, " +
-      dy +
-      "px, " +
-      depth +
-      "px)";
-
-    // console.log({ css });
-
-    details[i].node.style.transform =
-      "scale(" +
-      (1 - depth) +
-      ") translate3d(" +
-      dx +
-      "px, " +
-      dy +
-      "px, " +
-      depth +
-      "px)";
-  }
+    const dy = detail.sticky ? -(clip.scrollHeight - parallaxStart - height) * (1 - scale) : (parallaxStart - depth * (height - clip.clientHeight)) * scale;
+    detail.node.style.transform = 'scale(' + (1 - depth) + ') translate3d(' + dx + 'px, ' + dy + 'px, ' + depth + 'px)';
+  });
 }
 
 export { initializeParallax };
