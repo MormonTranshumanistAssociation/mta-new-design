ab17f9d9d3b674b4c99d9d184b929b707f6023d8
Carl Youngblood <carl@blockscale.net>
2022-02-11 00:33:59 -0700

    Update to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,39 @@
-const colors = require('tailwindcss/colors')
-  
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'index.html',
-    ]
-  },
-  darkMode: 'media', // or 'media' or 'class'
+  content: ['index.html'],
   theme: {
-    extend: {
-      colors: {
-        teal: colors.teal,
-        cyan: colors.cyan,
-      },
-    },
-  },
-  variants: {
-    extend: {},
+    colors: ({ colors }) => ({
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    }),
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
   ],
-}
+};
